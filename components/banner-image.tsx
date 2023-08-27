1a5a6d64dae6fd795a298cab61873afbbc92e54a
Thomas Taylor <thmsmtylr@gmail.com>
2023-08-27 20:59:02 +1000

    feature: more work

--- components/banner-image.tsx
+++ components/banner-image.tsx
@@ -75,6 +75,7 @@ export function BannerImages({ images }: { images: BannerImageProps }) {
               alt={image.alt}
               width={500}
               height={300}
+              quality={50}
               priority
             />
           </motion.div>
