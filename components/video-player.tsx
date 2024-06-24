801749803079e053f6f8b6d741aa05dd646600b5
vanbrew <61304584+vanbrew@users.noreply.github.com>
2024-06-24 16:18:13 +1000

    added pointer events in for testing

--- components/video-player.tsx
+++ components/video-player.tsx
@@ -25,7 +25,7 @@ export function VideoPlayer({
         url={url}
         width="100%"
         height="100%"
-        className="h-full w-full"
+        className="pointer-events-none h-full w-full"
         config={{
           youtube: { playerVars: { disablekb: 1 } },
           file: {
