d49b4d9065bd5fdb801041e3af01dc3b008424ad
Thomas Taylor <thmsmtylr@gmail.com>
2024-06-24 17:51:08 +1000

    fix: video

--- app/page.tsx
+++ app/page.tsx
@@ -37,16 +37,14 @@ export default async function Page() {
   return (
     <main className="home largepadding overflow-hidden bg-hydw-vanilla">
       <section className="relative z-20 h-[60vh] w-full overflow-hidden lg:h-screen">
-        <div className="pointer-events-none touch-none">
-          <VideoPlayer
-            // Test link
-            url={showreel?.url || ""}
-            playing={true}
-            controls={false}
-            muted={true}
-            loop={true}
-          />
-        </div>
+        <VideoPlayer
+          // Test link
+          url={showreel?.url || ""}
+          playing={true}
+          controls={false}
+          muted={true}
+          loop={true}
+        />
         <BouncingLogo />
       </section>
       <div className="page-grid wrapper bg-hydw-vanilla">
