ce9a49c54d97bad52e1b78d888921af3d9d08f14
Thomas Taylor <thmsmtylr@gmail.com>
2024-06-24 15:33:10 +1000

    fix: video

--- components/video-player.tsx
+++ components/video-player.tsx
@@ -31,6 +31,7 @@ export function VideoPlayer({
           file: {
             attributes: {
               controlsList: "nofullscreen",
+              disablePictureInPicture: true,
             },
           },
         }}
