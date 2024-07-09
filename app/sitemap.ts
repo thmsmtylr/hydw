919e566e1c6d8082844e01a21fc107e9420dd8a7
Thomas Taylor <thmsmtylr@gmail.com>
2024-07-09 20:09:57 +1000

    fix: sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -6,26 +6,38 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: ROOT_URL,
       lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
     },
     {
       url: `${ROOT_URL}/about`,
       lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
     },
     {
       url: `${ROOT_URL}/television`,
       lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
     },
     {
       url: `${ROOT_URL}/commercial"`,
       lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
     },
     {
       url: `${ROOT_URL}/grouse-house`,
       lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
     },
     {
       url: `${ROOT_URL}/contact`,
       lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.8,
     },
   ];
 }
