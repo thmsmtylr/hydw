e0b2354c6faa7a08cd4e0568915044bc577fa5e6
thomasspringboards <thomas@springboards.ai>
2025-08-14 17:37:07 +1000

    patch build

--- components/featured-scroll-rotate.tsx
+++ components/featured-scroll-rotate.tsx
@@ -1,10 +1,23 @@
 "use client";
-import { ScrollRotate, ScrollRotateProps } from "react-scroll-rotate";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 export function FeaturedScrollRotate({
-  children,
+	children,
 }: {
-  children: ScrollRotateProps["children"] | any;
+	children: React.ReactNode;
 }) {
-  return <ScrollRotate animationDuration={0.5} loops={1}>{children}</ScrollRotate>;
+	const ref = useRef<HTMLDivElement>(null);
+	const { scrollYProgress } = useScroll({
+		target: ref,
+		offset: ["start end", "end start"],
+	});
+
+	const rotate = useTransform(scrollYProgress, [0, 1], [0, 360]);
+
+	return (
+		<motion.div ref={ref} style={{ rotate }} className="will-change-transform">
+			{children}
+		</motion.div>
+	);
 }
